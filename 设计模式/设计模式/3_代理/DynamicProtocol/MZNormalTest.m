ac0d8864864fe4bf3feeb7b97cfff1301a73b50a
“mengzhe” <mengzhe360@163.com>
2020-01-08 00:54:43 +0800

    1、动态代理拦截崩溃

---
+++ 设计模式/设计模式/3_代理/DynamicProtocol/MZNormalTest.m
@@ -0,0 +1,31 @@
+//
+//  MZNormalTest.m
+//  设计模式
+//
+//  Created by 孟哲 on 2020/1/7.
+//  Copyright © 2020 孟哲. All rights reserved.
+//
+
+#import "MZNormalTest.h"
+
+@implementation MZNormalTest
+
+- (void)doSomething
+{
+    NSLog(@"MZNormalTest-doSomething");
+}
+- (void)doOtherThing
+{
+     NSLog(@"MZNormalTest-doOtherThing");
+}
+//- (void)optionalThing
+//{
+//     NSLog(@"MZNormalTest-optionalThing");
+//}
+- (void)mzOptionalThing
+{
+     NSLog(@"MZNormalTest-mzOptionalThing");
+}
+
+
+@end
--- 设计模式/设计模式/ViewController.m
+++ 设计模式/设计模式/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 #import "MZHMOne.h"
 #import "MZHMTwo.h"
+#import "MZDynamicProxy.h"
+#import "MZNormalObject.h"
+#import "MZNormalTest.h"
 
 @interface ViewController ()
 
@@ -25,13 +28,37 @@
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
     
+    [self dynamicProtocol];
+    
+}
+
+
+/// 2、模板模式
+- (void)templateMode
+{
     MZHMOne *one = [MZHMOne new];
     [one setAlarm:true];
     [one run];
     
     MZHMTwo *two = [MZHMTwo new];
     [two run];
+}
+
+
+/// 3、动态代理
+- (void)dynamicProtocol
+{
+    Class class = NSClassFromString(@"MZNormalTest");
+    id vc = [[class alloc] init];
+    id<MZDynamicProtocol> obj = (id)[[MZDynamicProxy alloc] initWithObject:vc];
     
+//        id<MZDynamicProtocol> obj = (id)[[MZNormalObject alloc] init];
+//        id<MZDynamicProtocol> obj = (id)[[MZDynamicProxy alloc] initWithObject:(id)[[MZNormalObject alloc] init]];
+
+    [obj doSomething];
+    [obj doOtherThing];
+    [obj optionalThing];
+    [(MZNormalTest *)vc mzOptionalThing];
 }
 
 @end
